1e32a639398a2c71fce36b0cae90eddf5f2e6006
Chi-kwan Chan <chanc@arizona.edu>
2022-01-30 01:33:11 -0700

    Implement the quadratic formula

--- quadratic.py
+++ quadratic.py
@@ -23,6 +23,9 @@
 #
 #    https://github.com/uarizona-2022spring-phys105a/phys105a/blob/main/03/Handson.ipynb
 #
+# Please look for "TODO:" for the places that you are suppose to make
+# change.
+#
 # Note, however, in order to explain how this script works, we need to
 # jump to different part of the code non-linearly.  To make it easily
 # to find the code, we use a long block of #====...==== to indicate
@@ -32,13 +35,61 @@
 # between `program` and `script`.  So we will use these two terms
 # interchangeablely in this file.
 #
-# To start, let's go to THE END of this script and look for the "1. Main block"
+# To start, let's go to THE END of this script and look for the "1. MAIN BLOCK"
 #
 #==============================================================================
+# 2. QUADRATIC EQUATION SOLVER
+#
+# NOTE: please read block "1. MAIN BLOCK" before you read this block.
+#
+# Here, we want to program a function that solve the quadratic
+# equation using the quadratic formula.  To demostrate the traps in
+# numerical analysis and computational scinece, we want to create
+# multiple versions that we can compare with.
 
+# import the square-root function from the standard math library
+from math import sqrt
+
+# The zeroth version is a straightforward implementation of the
+# quadratic formula.
+def quadratic0(a, b, c):
+    x1 = (-b + sqrt(b**2 - 4 * a * c)) / (2 * a)
+    x2 = (-b - sqrt(b**2 - 4 * a * c)) / (2 * a)
+    return (x1, x2)
+
+# Make sure that've uncommented the last two lines in the "1. MAIN
+# BLOCK".  And run
+#
+#    ./quadratic.py 1 2 1
+#    ./quadratic.py 1 -2 1
+#    ./quadratic.py 1 4 3
+#    ./quadratic.py 1 -4 3
+#
+# Yeah!  The program seems to work.
+
+#------------------------------------------------------------------------------
+# Now, try the following case:
+#
+#    ./quadratic.py 0 1 2
+#
+# This is a linear equation x + 2 == 0 and we know the solutoin is
+# simply -2.  However, quadratic0() simply throw an ZeroDivisionError
+# exception.
+#
+# To avoid this problem, let's develop an improved version of the
+# quadratic formula.  We may even reuse the old quadratic0() function:
+#
+# TODO: fill in the correct condition in the following function, and
+# update the second last line to use quadratic1() instead of
+# quadratic0().
+def quadratic1(a, b, c):
+    if ________:
+        return (-c / b,)
+    else:
+        return quadratic0(a, b, c)
 
 #==============================================================================
-# 1. Main block
+# 1. MAIN BLOCK
 #
 # In software engineering, it is useful to break a program into
 # multiple smaller functions so you can test each function
@@ -95,4 +146,13 @@ a = args.a
 b = args.b
 c = args.c
 
-print("Sovling the quadratic equation:", a, " x^2 +", b, "x +", c, "== 0")
+print("Sovling the quadratic equation:", a, "x^2 +", b, "x +", c, "== 0")
+
+# However, this script cannot solve anything yet.  To start
+# programming a quadratic equation solver, you need to
+#
+# TODO: uncomment the following lines
+#
+# and then jump to the block "2. QUADRATIC EQUATION SOLVER"
+#sol = quadratic0(a, b, c)
+#print("Solutions", sol)
